0fa0131ebb34cb7ee05276ebaae81ddafce40e81
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-30 10:29:08 +0300

    Brew formula update for bringgster version v0.3.1

--- Formula/bringgster.rb
+++ Formula/bringgster.rb
@@ -3,16 +3,16 @@ require_relative "../lib/custom_download_strategy"
 class Bringgster < Formula
   desc ""
   homepage "https://github.com/bringg/bringgster"
-  version "0.3.1-pre"
+  version "0.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bringg/bringgster/releases/download/v0.3.1-pre/bringgster_0.3.1-pre_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "ab569b050768561cb5e03afc28d3ab2208aa15cda838f02b15586f290b311d88"
+    url "https://github.com/bringg/bringgster/releases/download/v0.3.1/bringgster_0.3.1_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "b6a89ee3fef0110d34a8f428ecbac292b39b676e6a6573dfb4f0fb3a598d2bd0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/bringg/bringgster/releases/download/v0.3.1-pre/bringgster_0.3.1-pre_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "fff58ad9859900c0ebbe27f11a8207d3c36ae23735c4e5dcc6422fdb680e3d64"
+      url "https://github.com/bringg/bringgster/releases/download/v0.3.1/bringgster_0.3.1_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "23da560de69d916e64012da84ebff49cf1063fd16bb48e9d6041c8a36b4e35e8"
     end
   end
 
